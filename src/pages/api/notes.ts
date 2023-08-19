6857f0f5f2e4aa507435abb5013ac2ee1fc5be31
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2023-08-19 15:32:04 -0300

    Adiciona arquivos base

---
+++ src/pages/api/notes.ts
@@ -0,0 +1,44 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("./db.sqlite");
+db.run("CREATE TABLE notes (description TEXT)");
+
+type Data = {
+  name?: string;
+  message?: string;
+  notes?: any[];
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === "POST") {
+    const statement = db.prepare("INSERT INTO notes (description) VALUES (?)");
+    statement.run(req.body.description);
+    statement.finalize();
+    return res.status(201).json({ message: "success" });
+  }
+
+  if (req.method === "GET") {
+    db.all("SELECT rowid AS id, description FROM notes", (err, rows) => {
+      return res.status(200).json({ notes: rows });
+    });
+  }
+
+  if (req.method === "DELETE") {
+    db.run("DELETE FROM notes WHERE rowid = (?)", req.body.id);
+    return res.status(200).json({ message: "success" });
+  }
+
+  if (req.method === "PUT") {
+    db.run(
+      "UPDATE notes SET description = (?) WHERE rowid = (?)",
+      req.body.description,
+      req.body.id
+    );
+    return res.status(200).json({ message: "success" });
+  }
+}
